7a79fa233c28380e489342369fec88d6299e4df3
Adam Surak <adam@surak.eu>
2014-04-14 14:29:20 +0300

    Base megabuck script with actions

---
+++ megaback.py
@@ -0,0 +1,40 @@
+from mega import Mega
+import sys
+
+script, action = sys.argv
+
+allowed_actions = ["upload","delete","cleanup"]
+
+if action not in allowed_actions:
+  print "Unknown command"
+  sys.exit(1)
+
+mega = Mega()
+
+m = mega.login("","")
+
+if action == "upload":
+  filename = sys.argv[2]
+  file = m.upload(filename)
+  print m.get_upload_link(file)
+  sys.exit(0)
+
+elif action == "delete":
+  filename = sys.argv[2]
+  if "https" in filename:
+    m.destroy_url(filename)
+  else:
+    m.destroy(filename)
+  sys.exit(0)
+
+elif action == "cleanup":
+  files = m.get_files()
+
+  for file,atr in files.items():
+    if "k" in atr.keys():
+      try:
+        m.destroy(file)
+      except ValueError:
+        print "Error deleting file {0}".format(file)
+
+  sys.exit(0)
